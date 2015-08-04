a858a144e94ade0d7a6aed9ab2e57b6ab32668c0
Jason Lane <quizathaderat@yahoo.com>
2015-08-04 13:25:30 -0700

    Test.js now requires title_case.js

--- test.js
+++ test.js
@@ -1,21 +1,5 @@
 
-String.prototype.title_case = function () {
-  var words = this.split(' ');
-  var word = '';
-  //console.log(words);
-  if (words.length > 1) {
-    console.log('hi');
-    for (var i=0; i < words.length; i++) {
-      word = word.concat( words[i].charAt(0).toUpperCase() + words[i].slice(1) + ' ');
-    }
-    return word.trim();
-
-  }
-  else {
-    return this.charAt(0).toUpperCase() + this.slice(1);
-  }
-};
-
+require('./title_case.js');
 
 var assert = require("assert")
 describe( 'String#title_case', function() {
---
+++ title_case.js
@@ -0,0 +1,16 @@
+String.prototype.title_case = function () {
+  var words = this.split(' ');
+  var word = '';
+  //console.log(words);
+  if (words.length > 1) {
+    console.log('hi');
+    for (var i=0; i < words.length; i++) {
+      word = word.concat( words[i].charAt(0).toUpperCase() + words[i].slice(1) + ' ');
+    }
+    return word.trim();
+
+  }
+  else {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+  }
+};
