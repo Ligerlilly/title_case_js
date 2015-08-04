195df9fed06ab43ee91a1ae27444dd0314504f3b
Jason Lane <quizathaderat@yahoo.com>
2015-08-04 13:23:41 -0700

    Capitalizes first letter of a string and first letter of all words in a multi-word string

--- test.js
+++ test.js
@@ -1,14 +1,30 @@
 
 String.prototype.title_case = function () {
-  return 'Hi';
+  var words = this.split(' ');
+  var word = '';
+  //console.log(words);
+  if (words.length > 1) {
+    console.log('hi');
+    for (var i=0; i < words.length; i++) {
+      word = word.concat( words[i].charAt(0).toUpperCase() + words[i].slice(1) + ' ');
+    }
+    return word.trim();
+
+  }
+  else {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+  }
 };
 
 
 var assert = require("assert")
-describe('String#title_case', function() {
-
-    it('should capitalize first letter of a string', function () {
+describe( 'String#title_case', function() {
+  it( 'should capitalize first letter of a string', function () {
       assert.equal( "hi".title_case(), "Hi" );
-
   });
+
+  it( 'should capitalize all words in a multi-word string', function(){
+    assert.equal( 'Hi there'.title_case(), "Hi There" )
+  })
+
 });
